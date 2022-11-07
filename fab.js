14a5b57fc89661b665e5a924b12371bb01eded09
Paul Tarvydas <guitarvydas@gmail.com>
2022-11-06 23:24:46 -0500

    new angle brackets

--- fab.js
+++ fab.js
@@ -134,210 +134,150 @@ var varNameStack = [];
 // }
 // ]]
 
-const semObject = {
+const semObject = 
+{
+top: function (_ws1,_name,_ws2,_lb,_ws3,_rule,_ws4,_rb,_ws5,_more) {
+_ruleEnter ("top");
+var ws1 = _ws1._fab ();
+var name = _name._fab ();
+var ws2 = _ws2._fab ();
+var lb = _lb._fab ();
+var ws3 = _ws3._fab ();
+var rule = _rule._fab ().join ('');
+var ws4 = _ws4._fab ();
+var rb = _rb._fab ();
+var ws5 = _ws5._fab ();
+var more = _more._fab ().join ('');
 
-    top : function (_ws1,_name,_ws2,_lb,_ws4,_rule,_ws5,_rb,_ws3,_more) { 
-        _ruleEnter ("top");
-
-        var ws1 = _ws1._fab ();
-        var name = _name._fab ();
-        var ws2 = _ws2._fab ();
-        var lb = _lb._fab ();
-        var ws4 = _ws4._fab ();
-        var rule = _rule._fab ().join ('');
-        var ws5 = _ws5._fab ();
-        var rb = _rb._fab ();
-        var ws3 = _ws3._fab ();
-        var more = _more._fab ().join ('');
-        var _result = `{
+_ruleExit ("top");
+return `
+{
 ${rule}${more}
+    _terminal: function () { return this.sourceString; },
+    _iter: function (...children) { return children.map(c => c._fmt ()); },
+    spaces: function (x) { return this.sourceString; },
+    space: function (x) { return this.sourceString; }
+}
+`;
+},
+more: function (_name,_ws1,_lb,_ws2,_rule,_ws3,_rb,_ws4) {
+_ruleEnter ("more");
+var name = _name._fab ();
+var ws1 = _ws1._fab ();
+var lb = _lb._fab ();
+var ws2 = _ws2._fab ();
+var rule = _rule._fab ().join ('');
+var ws3 = _ws3._fab ();
+var rb = _rb._fab ();
+var ws4 = _ws4._fab ();
+
+_ruleExit ("more");
+return `
+${rule}
+`;
+},
+rule: function (_lhs,_ws1,_keq,_ws2,_rws) {
+_ruleEnter ("rule");
+var lhs = _lhs._fab ();
+var ws1 = _ws1._fab ();
+var keq = _keq._fab ();
+var ws2 = _ws2._fab ();
+var rws = _rws._fab ();
+
+_ruleExit ("rule");
+return `${lhs}${rws}
+_ruleExit ("${getRuleName ()}");
+},
+`;
+},
+RuleLHS: function (_name,_lb,_Params,_rb) {
+_ruleEnter ("RuleLHS");
+var name = _name._fab ();
+var lb = _lb._fab ();
+var Params = _Params._fab ().join ('');
+var rb = _rb._fab ();
+
+_ruleExit ("RuleLHS");
+return `${name}: function (${extractFormals(Params)}) {\n_ruleEnter ("${name}");${setRuleName (name)}${Params}
+`;
+},
+rewriteString: function (_sb,_cs,_se,_ws) {
+_ruleEnter ("rewriteString");
+var sb = _sb._fab ();
+var cs = _cs._fab ().join ('');
+var se = _se._fab ();
+var ws = _ws._fab ();
+
+_ruleExit ("rewriteString");
+return `return \`${cs}\`;`;
+},
+char_eval: function (_lb,_name,_rb) {
+_ruleEnter ("char_eval");
+var lb = _lb._fab ();
+var name = _name._fab ();
+var rb = _rb._fab ();
+
+_ruleExit ("char_eval");
+return `\$\{${name}\}`;
+},
+char_raw: function (_c) {
+_ruleEnter ("char_raw");
+var c = _c._fab ();
+
+_ruleExit ("char_raw");
+return `${c}`;
+},
+name: function (_c,_cs) {
+_ruleEnter ("name");
+var c = _c._fab ();
+var cs = _cs._fab ().join ('');
+
+_ruleExit ("name");
+return `${c}${cs}`;
+},
+nameRest: function (_c) {
+_ruleEnter ("nameRest");
+var c = _c._fab ();
+
+_ruleExit ("nameRest");
+return `${c}`;
+},
+Param_plus: function (_name,_k) {
+_ruleEnter ("Param_plus");
+var name = _name._fab ();
+var k = _k._fab ();
+
+_ruleExit ("Param_plus");
+return `\nvar ${name} = _${name}._glue ().join ('');`;
+},
+Param_star: function (_name,_k) {
+_ruleEnter ("Param_star");
+var name = _name._fab ();
+var k = _k._fab ();
+
+_ruleExit ("Param_star");
+return `\nvar ${name} = _${name}._glue ().join ('');`;
+},
+Param_opt: function (_name,_k) {
+_ruleEnter ("Param_opt");
+var name = _name._fab ();
+var k = _k._fab ();
+
+_ruleExit ("Param_opt");
+return `\nvar ${name} = _${name}._glue ().join ('');`;
+},
+Param_flat: function (_name) {
+_ruleEnter ("Param_flat");
+var name = _name._fab ();
+
+_ruleExit ("Param_flat");
+return `\nvar ${name} = _${name}._glue ();`;
+},
+
     _terminal: function () { return this.sourceString; },
     _iter: function (...children) { return children.map(c => c._fab ()); },
     spaces: function (x) { return this.sourceString; },
     space: function (x) { return this.sourceString; }
 }
-`; 
-        _ruleExit ("top");
-        return _result; 
-    },
-
-    more : function (_name,_ws2,_lb,_ws4,_rule,_ws5,_rb,_ws3) { 
-        _ruleEnter ("top");
-
-        var name = _name._fab ();
-        var ws2 = _ws2._fab ();
-        var lb = _lb._fab ();
-        var ws4 = _ws4._fab ();
-        var rule = _rule._fab ().join ('');
-        var ws5 = _ws5._fab ();
-        var rb = _rb._fab ();
-        var ws3 = _ws3._fab ();
-        var _result = `
-${rule}
-`; 
-        _ruleExit ("top");
-        return _result; 
-    },
-
-
-    ////
-    
-    // rule [lhs ws1 keq ws2 rws] = [[${lhs}${rws}
-    // _ruleExit ("${getRuleName ()}");
-    // },
-    // ]]
-
-
-    rule : function (_lhs,_ws1,_keq,_ws2,_rws) { 
-        _ruleEnter ("rule");
-
-        var lhs = _lhs._fab ();
-        var ws1 = _ws1._fab ();
-        var keq = _keq._fab ();
-        var ws2 = _ws2._fab ();
-        var rws = _rws._fab ();
-        var _result = `${lhs}
-_ruleExit ("${getRuleName ()}");
-return ${rws}
-},
-`; 
-        _ruleExit ("rule");
-        return _result; 
-    },
-    ////
-    
-    // RuleLHS [name lb @Params rb] = [[${name}: function (${extractFormals(Params)}) {\n_ruleEnter ("${name}");${setRuleName (name)}${Params}
-    // ]]
-    RuleLHS : function (_name,_lb,_Params,_rb) { 
-        _ruleEnter ("RuleLHS");
-
-        var name = _name._fab ();
-        var lb = _lb._fab ();
-        var Params = _Params._fab ().join ('');
-        var rb = _rb._fab ();
-        var _result = `${name}: function (${extractFormals(Params)}) {\n_ruleEnter ("${name}");${setRuleName (name)}${Params}
-`; 
-        _ruleExit ("RuleLHS");
-        return _result; 
-    },
-    
-    ////
-
-
-    // rewriteString [sb @cs se ws] = [[return \`${cs}\`;]]
-    rewriteString : function (_sb,_cs,_se,_ws) { 
-        _ruleEnter ("rewriteString");
-
-        var sb = _sb._fab ();
-        var cs = _cs._fab ().join ('');
-        var se = _se._fab ();
-        var ws = _ws._fab ();
-        var _result = `\`${cs}\`;`; 
-        _ruleExit ("rewriteString");
-        return _result; 
-    },
-
-
-    ////
-    // char_eval [lb name rb] = [[\$\{${name}\}]]
-    // char_raw [c] = [[${c}]]
-    char_eval : function (_lb,_cs,_rb) { 
-        _ruleEnter ("char_eval");
-
-        var lb = _lb._fab ();
-        var name = _cs._fab ().join ('');
-        var rb = _rb._fab ();
-        var _result = `\$\{${name}\}`; 
-        _ruleExit ("char_eval");
-        return _result; 
-    },
-    
-    char_raw : function (_c) { 
-        _ruleEnter ("char_raw");
-
-        var c = _c._fab ();
-        var _result = `${c}`; 
-        _ruleExit ("char_raw");
-        return _result; 
-    },
-    ////
-    
-    // name [c @cs] = [[${c}${cs}]]
-    // nameRest [c] = [[${c}]]
-
-    name : function (_c,_cs) { 
-        _ruleEnter ("name");
-
-        var c = _c._fab ();
-        var cs = _cs._fab ().join ('');
-        var _result = `${c}${cs}`; 
-        _ruleExit ("name");
-        return _result; 
-    },
-    
-    nameRest : function (_c) { 
-        _ruleEnter ("nameRest");
-
-        var c = _c._fab ();
-        var _result = `${c}`; 
-        _ruleExit ("nameRest");
-        return _result; 
-    },
-
-    ////
-
-
-    // Param_plus [name k] = [[\nvar ${name} = _${name}._fab ().join ('');]]
-    // Param_star [name k] = [[\nvar ${name} = _${name}._fab ().join ('');]]
-    // Param_opt [name k] = [[\nvar ${name} = _${name}._fab ().join ('');]]
-    // Param_flat [name] = [[\nvar ${name} = _${name}._fab ();]]
-
-
-    Param_plus : function (_name,_k) { 
-        _ruleEnter ("Param_plus");
-
-        var name = _name._fab ();
-        var k = _k._fab ();
-        var _result = `\nvar ${name} = _${name}._fab ().join ('');`; 
-        _ruleExit ("Param_plus");
-        return _result; 
-    },
-    
-    Param_star : function (_name,_k) { 
-        _ruleEnter ("Param_star");
-
-        var name = _name._fab ();
-        var k = _k._fab ();
-        var _result = `\nvar ${name} = _${name}._fab ().join ('');`; 
-        _ruleExit ("Param_star");
-        return _result; 
-    },
-    
-    Param_opt : function (_name,_k) { 
-        _ruleEnter ("Param_opt");
-
-        var name = _name._fab ();
-        var k = _k._fab ();
-        var _result = `\nvar ${name} = _${name}._fab ().join ('');`; 
-        _ruleExit ("Param_opt");
-        return _result; 
-    },
-    
-    Param_flat : function (_name) { 
-        _ruleEnter ("Param_flat");
-
-        var name = _name._fab ();
-        var _result = `\nvar ${name} = _${name}._fab ();`; 
-        _ruleExit ("Param_flat");
-        return _result; 
-    },
-    
-    ////
-
-    _terminal: function () { return this.sourceString; },
-    _iter: function (...children) { return children.map(c => c._fab ()); },
-    spaces: function (x) { return this.sourceString; },
-    space: function (x) { return this.sourceString; }
-};
-// yyy
+;
 
